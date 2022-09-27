2c45581a567ef02d699c407b70e6c913059f71cc
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-09-26 19:44:43 -0500

    feat: Create template to display subscription info

--- src/main/static/js/script.js
+++ src/main/static/js/script.js
@@ -7,12 +7,30 @@ fetch("/config/")
   // Initialize Stripe.js
   const stripe = Stripe(data.publicKey);
 
-  // Event handler
-  let submitBtn = document.querySelector("#submitBtn");
+  // Event handler for monthly subscription
+  let submitBtn = document.querySelector("#submitBtn1");
   if (submitBtn !== null) {
     submitBtn.addEventListener("click", () => {
     // Get Checkout Session ID
     fetch("/create-checkout-session/1/")
+      .then((result) => { return result.json(); })
+      .then((data) => {
+        console.log(data);
+        // Redirect to Stripe Checkout
+        return stripe.redirectToCheckout({sessionId: data.sessionId})
+      })
+      .then((res) => {
+        console.log(res);
+      });
+    });
+  }
+
+  // Event handler for yearly subscription
+  let submitBtn2 = document.querySelector("#submitBtn2");
+  if (submitBtn2 !== null) {
+    submitBtn2.addEventListener("click", () => {
+    // Get Checkout Session ID
+    fetch("/create-checkout-session/2/")
       .then((result) => { return result.json(); })
       .then((data) => {
         console.log(data);
